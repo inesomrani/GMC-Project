ebe07d65cc0358dc0c557c92553c9b22f7b64bf1
inesomrani <61356558+inesomrani@users.noreply.github.com>
2020-04-25 15:49:44 +0100

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+$(".img1").hover(function(){
+    $(".btn1").toggleClass('btnHover')
+})
+
+$(".img2").hover(function(){
+    $(".btn2").toggleClass('btnHover')
+})
+
+$(".img3").hover(function(){
+    $(".btn3").toggleClass('btnHover')
+})
+$(".img4").hover(function(){
+    $(".btn4").toggleClass('btnHover')
+})
